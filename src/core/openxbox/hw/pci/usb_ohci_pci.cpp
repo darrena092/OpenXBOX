53b47d4a72d54a789c722c6263a9c179475e159d
StrikerX3 <ivan.rober@gmail.com>
2018-03-19 18:13:16 -0300

    Added QEMU copyright header to USB OHCI code

--- src/core/openxbox/hw/pci/usb_ohci_pci.cpp
+++ src/core/openxbox/hw/pci/usb_ohci_pci.cpp
@@ -1,3 +1,33 @@
+/*
+ * Portions of the code are based on QEMU's USB OHCI Emulation.
+ * The original copyright header is included below.
+ */
+/*
+ * QEMU USB OHCI Emulation
+ * Copyright (c) 2004 Gianni Tedesco
+ * Copyright (c) 2006 CodeSourcery
+ * Copyright (c) 2006 Openedhand Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * TODO:
+ *  o Isochronous transfers
+ *  o Allocate bandwidth in frames properly
+ *  o Disable timers when nothing needs to be done, or remove timer usage
+ *    all together.
+ *  o BIOS work to boot from USB storage
+ */
 #include "usb_ohci_pci.h"
 #include "openxbox/log.h"
 #include "openxbox/util.h"
